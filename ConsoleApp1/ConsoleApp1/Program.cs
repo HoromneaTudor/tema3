41d284f39951fe417bff18d7f17ec7726bef81eb
HoromneaTudor <62078729+HoromneaTudor@users.noreply.github.com>
2020-04-03 19:25:35 +0300

    ex3

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidentaStudenti2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string optiune;
+            Student s = new Student("Tudor");
+            while (true)
+            {
+                Console.WriteLine( "S: Citire note de la tastatura ca un sir de caractere");
+                Console.WriteLine("T: Citire note de la tastatura cate o nota pe rand");
+                Console.WriteLine( "C: Citire note din linia de comanda");
+                Console.WriteLine( "A: Afisare note");
+                Console.WriteLine( "I: Info autor");
+                Console.WriteLine( "X: Iesire");
+                Console.WriteLine();
+                Console.Write("Dati optiunea:");
+                optiune = Console.ReadLine();
+
+                switch (optiune.ToUpper())
+                {
+                    case "S":
+
+                        string note;
+                        Console.WriteLine("Dati notele separate prin spatiu:");
+                        note = Console.ReadLine();
+
+
+                        break;
+                    case "A":
+
+                        s.ConversieLaSir();
+                        break;
+                    case "T":
+                        Console.Write("Dati numarul de note:");
+                        int numar = Convert.ToInt32(Console.ReadLine());
+
+                        int[] noteINT = new int[numar];
+                        for (int i = 0; i < numar; i++)
+                        {
+                            Console.Write("Nota numarul {0}:", i);
+                            noteINT[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+
+                        break;
+                    case "C":
+
+                        s.SetNote("1 2 3 4 5 , 4 5 6 7 8 , 3 4 5");
+
+
+                        break;
+                    case "X":
+                        Environment.Exit(0);
+                        break;
+                    case "I":
+                        Console.WriteLine("Horomnea Tudor 3122A");
+                        break;
+                    default:
+                        Console.WriteLine("Optiune inexistenta.");
+                        break;
+                }
+                Console.ReadKey();
+                Console.Clear();
+               
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvidentaStudenti2
+{
+    public class Student
+    {
+     
+        string nume;
+        int[,] note;
+
+
+    
+        public Student()
+        {
+        }
+
+        public Student(string _nume)
+        {
+            nume = _nume;
+        }
+
+        public void SetNote(string sirNote)
+        {
+            note = new int[4, 15];
+ 
+            string[] notes = new string[4];
+            notes = sirNote.Split(',');
+            string[] temp;
+            int i = 0;
+            foreach (string notari in notes)
+            {
+                temp = notari.Split(' ');
+
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    Int32.TryParse(temp[j], out note[i, j]);
+                   
+                }
+                i++;
+
+            }
+        }
+
+        public void SetNote(int[,] _note)
+        {
+            note = new int[4, 15];
+            note = _note;
+
+        }
+        public void ConversieLaSir()
+        {
+            string sNote = "Nu exista (Nu ati apelat metoda **setNote**)";
+            if (note != null)
+            {
+                sNote = string.Join(",", note);
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Notele pe anul {0} sunt: ", i + 1);
+                for (int j = 0; j < 15; j++)
+                    Console.Write("{0}", note[i, j]);
+                Console.WriteLine();
+            }
+            
+        }
+        
+        
+
+    }
+}
